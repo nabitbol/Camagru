23d5898280e7f58927ef1ea4747fbaff48bc5c3c
Override <override@watchtower>
2024-08-21 12:38:16 +0200

    feat: added signup page

--- notes/pages.md
+++ notes/pages.md
@@ -1,11 +1,14 @@
 # Pages
 
-- [ ] Sign up
-- [ ] Sign up success
+- [x] Sign up
+  - [ ] Success
 - [ ] Verifiy E-mail
 - [x] Sign in
 - [x] Send password reset
+  - [ ] Success
 - [x] Reset password
+  - [ ] Success
+
 - [ ] Gallery
 - [ ] Image
 - [ ] Camera
--- src/apps/frontend/global-scripts/main.js
+++ src/apps/frontend/global-scripts/main.js
@@ -1,5 +1,6 @@
 document.camagru = {
   validateEmail: (email) => /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/.test(email),
+  validateUsername: (username) => /^[a-zA-Z0-9]$/.test(username),
   validatePassword: (password) => /^.*(?=.{8,})(?=.*[a-zA-Z])(?=.*\d)(?=.*[!#$%&? "]).*$/.test(password),
   fetch: async (method, url, body) => {
     console.log({ method, url, body })
