5309bf353dcc70971322083fabdf367ee24c69eb
noam abitbol <nabt@google.com>
2024-09-22 19:50:56 +0000

    feat(backend): added the ability to handle params in request url from with the colon annotation

--- src/common/myexpress/server.js
+++ src/common/myexpress/server.js
@@ -40,9 +40,37 @@ const Server = class {
     });
   }
 
+  #checkUrl(path, req) {
+
+    const pathRessources = path.split('/');
+    const reqUrlRessources = req.url.split('/');
+    let params = {};
+    
+    if(reqUrlRessources.length != pathRessources.length)
+      return false;
+    for (let i = 0; i < reqUrlRessources.length; i++) {
+
+      if (pathRessources[i][0] == ':') {
+        const param = pathRessources[i].slice(1)
+        params[param] = reqUrlRessources[i];
+        i++;
+      };
+  
+      if (reqUrlRessources[i] !== pathRessources[i])
+        return false;
+
+    };
+
+    req.params = params;
+    console.log(req.params);
+
+    return true;
+
+  }
+
   #callHandler(method, path, handlers) {
     this.server.on("request", (req, res) => {
-      if (req.method === method && req.url === path)
+      if (req.method === method && this.#checkUrl(path, req))
         addBodyRequestAndCallHandler(req, res, handlers);
     });
   }
